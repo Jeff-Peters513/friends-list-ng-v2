3f9a9eaa21fe9d10c0f53af1881d16dec27b0056
Jeff Peters <petersjeff288@gmail.com>
2020-05-11 13:34:34 -0400

    init commit

---
+++ src/app/model/friend.class.ts
@@ -0,0 +1,13 @@
+export class Friend {
+    firstName: string;
+    lastName: string;
+    phone: string;
+    email: string;
+    bff: boolean;
+
+    constructor(firstName: string="", lastName: string ="", phone: string= "",
+     email:string ="", bff: boolean = false){
+
+    }
+
+}
